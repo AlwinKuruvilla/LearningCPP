bef3d93c1de87e2e888bffafddf8cf282cc359e0
Alwin Kuruvilla <alwin@akxlgames.com>
2022-02-28 14:14:48 -0500

    Add notes about variables.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+
+int a, b = 5; // Global variables. Managed statically by the compiler. Allocated in the data segment of memory and freed only when the program ends.
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;m
+    std::string myStr; // Local (automatic) variable. Allocated in the stack segment of memory and freed when the function terminates.
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> myStr;
+    std::cout << "Your name is: " + myStr << std::endl;
     return 0;
 }
